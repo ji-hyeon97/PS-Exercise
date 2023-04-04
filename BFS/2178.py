35e24d9ead875239654534444302eee24a42b60e
= <79920930+ji-hyeon97@users.noreply.github.com>
2023-04-04 21:05:05 +0900

    2178 미로 탐색

---
+++ BFS/2178.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+n,m = map(int,sys.stdin.readline().split())
+graph = []
+for i in range(n):
+  data = list(map(str,sys.stdin.readline().rstrip()))
+  graph.append(data)
+
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+
+def bfs(i,j):
+  queue = deque()
+  queue.append((i,j))
+  while(queue):
+    x,y = queue.popleft()
+    for i in range(4):
+      nx = x+dx[i]
+      ny = y+dy[i]
+
+      if nx<0 or ny<0 or nx>=n or ny>=m:
+        continue
+
+      if graph[nx][ny] == '0':
+        continue
+
+      if graph[nx][ny] =='1':
+        graph[nx][ny] = int(graph[x][y])+1
+        queue.append((nx,ny))
+
+bfs(0,0)
+print(graph[n-1][m-1])
