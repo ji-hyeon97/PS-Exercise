d1374056a80db01a56ef1a5b32e152fcff0ff761
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-04-27 11:32:35 +0900

    [Silver III] Title: 회전하는 큐, Time: 68 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,28 +1,30 @@
 import sys
 from collections import deque
 
-n,m = map(int,sys.stdin.readline().split())
-data = list(map(int,sys.stdin.readline().split()))
 queue = deque()
-for i in range(1,n+1):
-  queue.append(i)
+n, m = map(int, sys.stdin.readline().split())
+data = list(map(int, sys.stdin.readline().split()))
+
+for i in range(1, n + 1):
+    queue.append(i)
 
 count = 0
 for i in data:
-  while(True):
-    target = queue.index(i)
-    
-    if queue[0] == i:
-      queue.popleft()
-      break
-      
-    if target<=len(queue)/2:
-      a = queue.popleft()
-      queue.append(a)
-      count+=1
-    else:
-      a = queue.pop()
-      queue.appendleft(a)
-      count+=1
-      
+    while True:
+        if queue[0] == i:
+            queue.popleft()
+            break
+
+        index = queue.index(i)
+
+        if index <= len(queue) / 2:
+            value = queue.popleft()
+            queue.append(value)
+
+        if index > len(queue) / 2:
+            value = queue.pop()
+            queue.appendleft(value)
+
+        count += 1
+
 print(count)
