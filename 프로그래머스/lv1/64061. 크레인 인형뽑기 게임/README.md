8d18fe6bfe9abf0b314b8fedefb9db28872ef1ab
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-23 23:42:59 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.76 ms
+메모리: 10.1 MB, 시간: 1.57 ms
 
 ### 구분
 
--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,16 +1,16 @@
 def solution(board, moves):
     answer = 0
-    box = []
-    height = len(board)
-    
-    for i in moves:
-        for j in range(height):
-            if board[j][i-1] != 0:
-                if box and box[-1] == board[j][i-1]:
-                    box.pop()
-                    answer+=2
-                else:
-                    box.append(board[j][i-1])
-                board[j][i-1] = 0
+    stack = []
+    for idx in moves:
+        idx = idx-1
+        for i in range(len(board)):
+            if len(stack) and stack[-1] == board[i][idx]:
+                board[i][idx] = 0
+                stack.pop()
+                answer+=2
+                break
+            if board[i][idx] != 0:
+                stack.append(board[i][idx])
+                board[i][idx] = 0
                 break
     return answer
