0299bb84cfe65c840c2f322fdff3bb51db54db36
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-24 01:41:59 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -4,29 +4,28 @@ def solution(numbers, hand):
     right = [3,2]
     for i in numbers:
         position = [(i-1)//3, (i-1)%3]
-        if i==0:
+        if i == 0:
             position = [3,1]
         if i in [1,4,7]:
             answer+='L'
             left = position
-        elif i in [3,6,9]:
+        if i in [3,6,9]:
             answer+='R'
             right = position
-        else:
-            dl = abs(position[0] - left[0]) + abs(position[1] - left[1])
-            dr = abs(position[0] - right[0]) + abs(position[1] - right[1])
-            
-            if dl > dr:
-                answer+='R'
-                right = position
-            elif dl < dr:
-                answer+='L'
+        if i in [2,5,8,0]:
+            dl = abs(left[0] - position[0]) + abs(left[1] - position[1])
+            dr = abs(right[0] - position[0]) + abs(right[1] - position[1])
+            if dl < dr:
                 left = position
-            else:
-                if hand == 'right':
-                    answer+='R'
-                    right = position
-                else:
+                answer+='L'
+            if dl > dr:
+                right = position
+                answer+='R'
+            if dl == dr:
+                if hand[0] == 'l':
+                    left = position
                     answer+='L'
-                    left = position        
+                else:
+                    right = position
+                    answer+='R'
     return answer
