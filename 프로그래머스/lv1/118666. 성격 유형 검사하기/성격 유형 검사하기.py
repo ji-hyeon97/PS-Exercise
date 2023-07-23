bef68ec9c1c6f803e445dff14b11bc325f33891c
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-24 00:30:57 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,16 +1,16 @@
 def solution(survey, choices):
     answer = ''
     data = {}
-    type = ['R','T','C','F','J','M','A','N']
-    for i in type:
+    alpha = ['R','T','C','F','J','M','A','N']
+    for i in alpha:
         data[i] = 0
+    
     for i in range(len(survey)):
-        if choices[i]==4:
-            continue
-        if choices[i] < 4:
-            data[survey[i][0]] = data[survey[i][0]]+(4-choices[i])
-        if choices[i] >4:
-            data[survey[i][1]] = data[survey[i][1]]+choices[i]-4
+        if choices[i] >= 4:
+            data[survey[i][1]] += choices[i] - 4
+        else:
+            data[survey[i][0]] += 4 - choices[i]
+    
     if data['R'] >= data['T']:
         answer+='R'
     else:
@@ -27,4 +27,5 @@ def solution(survey, choices):
         answer+='A'
     else:
         answer+='N'
+        
     return answer
