6efb4f73fc02db30196276a59ad3435f5a64fde6
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-24 11:03:39 +0900

    [unrated] Title: 공원 산책, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,29 +1,63 @@
 def solution(park, routes):
     answer = []
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    d = {'N': 0, 'E' : 1, 'S' : 2, 'W' : 3}
-    r = len(park)
-    c = len(park[0])
-    for i in range(r):
-        for j in range(c):
-            if park[i][j] == 'S':
-                a,b = i,j
-    for i in routes:
-        command = i.split(" ")
-        direction = command[0]
-        dis = int(command[1])
-        flag = 1
-        n,m = a,b
-        for _ in range(dis):
-            nx = n + dx[d[direction]] 
-            ny = m + dy[d[direction]]
-            if nx<0 or ny<0 or nx>=r or ny>=c or park[nx][ny]=='X':
-                flag = 0
-                break
-            n,m = nx,ny
-        if flag:
-            a,b = nx,ny
-            
-    answer = [a,b]
+    row = len(park)
+    col = len(park[0])
+    x,y = 0,0
+    
+    graph = [[] for _ in range(row)]
+    
+    idx = 0
+    for i in park:
+        for j in i:
+            graph[idx].append(j)
+        idx+=1     
+    for i in range(row):
+        for j in range(col):
+            if graph[i][j] == 'S':
+                x,y = i,j
+    for data in routes:
+        where, go = data.split(" ")
+        nx = x
+        ny = y
+        for _ in range(int(go)):
+            if where == 'E':
+                dx = nx
+                dy = ny+1
+                if dx<0 or dy<0 or dx>=row or dy>=col or graph[dx][dy] =='X':
+                    nx = x
+                    ny = y
+                    break
+                nx = dx
+                ny = dy
+            if where == 'N':
+                dx = nx-1
+                dy = ny
+                if dx<0 or dy<0 or dx>=row or dy>=col or graph[dx][dy] =='X':
+                    nx = x
+                    ny = y
+                    break
+                nx = dx
+                ny = dy
+            if where == 'S':
+                dx = nx+1
+                dy = ny
+                if dx<0 or dy<0 or dx>=row or dy>=col or graph[dx][dy] =='X':
+                    nx = x
+                    ny = y
+                    break
+                nx = dx
+                ny = dy
+            if where == 'W':
+                dx = nx
+                dy = ny-1
+                if dx<0 or dy<0 or dx>=row or dy>=col or graph[dx][dy] =='X':
+                    nx = x
+                    ny = y
+                    break
+                nx = dx
+                ny = dy
+        x = nx
+        y = ny
+        
+    answer = [x,y]
     return answer
