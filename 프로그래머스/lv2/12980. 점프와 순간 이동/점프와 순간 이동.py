d91e61460325eb42954c009d531dc474f83b4036
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-26 08:34:52 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    ans = 0
+    while True:
+        if n==0:
+            break
+        if n%2 == 0:
+            n = n//2
+        else:
+            n = n-1
+            ans+=1
+    return ans
