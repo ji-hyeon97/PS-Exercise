b2241408fa6ba66dcfdeb45bfe5f9a71fcbefea9
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-07-28 01:37:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.46 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,31 +3,30 @@ from collections import deque
 def solution(maps):
     answer = 0
     queue = deque()
-    dx = [0,0,-1,1]
-    dy = [1,-1,0,0]
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     row = len(maps)
     col = len(maps[0])
-    check = [[1 for _ in range(col)] for _ in range(row)]
     queue.append((0,0))
+    check = [[0 for _ in range(col)] for _ in range(row)]
     
-    while(queue):
+    while queue:
         x,y = queue.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx<0 or ny<0 or nx>=row or ny>= col:
+            nx = dx[i] + x
+            ny = dy[i] + y
+            
+            if nx<0 or ny<0 or nx>=row or ny>=col:
                 continue
-            if check[nx][ny] != 1:
+            
+            if maps[nx][ny] ==0 or check[nx][ny] ==1:
                 continue
-            if maps[nx][ny] == 0:
-                continue
-            if nx ==row and ny == col:
-                return check[nx][ny]
-            if maps[nx][ny] == 1 and check[nx][ny]==1:
-                check[nx][ny] = check[x][y] + 1
-                queue.append((nx,ny))
-    if check[row-1][col-1] == 1:
-        answer = -1
-    else:
-        answer = check[row-1][col-1]
-    return answer
+            
+            if maps[nx][ny] == 1 and check[nx][ny] == 0:
+                maps[nx][ny] = 1+maps[x][y]
+                check[nx][ny] = 1
+                queue.append((nx,ny)) 
+                
+    if maps[row-1][col-1] == 1:
+        return -1
+    return maps[row-1][col-1]
