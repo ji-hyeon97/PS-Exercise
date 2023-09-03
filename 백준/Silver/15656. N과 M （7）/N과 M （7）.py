d8dfdb54275861fd7a80c3a31d695d4a92d60a91
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2023-09-03 19:34:20 +0900

    [Silver III] Title: N과 M (7), Time: 3092 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,24 @@
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+check = list(map(int, sys.stdin.readline().split()))
+check.sort()
+data = []
+
+
+def recur(data):
+    if len(data) == b:
+        for i in data:
+            print(i, end=" ")
+        print("")
+    else:
+        for i in check:
+            data.append(i)
+            recur(data)
+            data.pop()
+
+
+for i in check:
+    data.append(i)
+    recur(data)
+    data = []
