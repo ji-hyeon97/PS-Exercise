cdb3022ed6f9d691b8f2ce4ed64d412adc0da151
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2024-01-22 21:40:39 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,15 +1,13 @@
 import sys
-
 n = int(sys.stdin.readline().rstrip())
-data = []
+graph = []
+
 for _ in range(n):
-  data.append(list(map(int,sys.stdin.readline().split())))
+    data = list(map(int,sys.stdin.readline().split()))
+    graph.append(data)
 
-dp = [0 for i in range(n+1)]
-
+dp = [0] * (n+1)
 for i in range(n):
-  for j in range(i+data[i][0],n+1):
-    if dp[j] < dp[i]+data[i][1]:
-      dp[j] = dp[i]+data[i][1]
-
+    for j in range(i+graph[i][0],n+1):
+        dp[j] = max(dp[j], dp[i]+graph[i][1])
 print(dp[-1])
