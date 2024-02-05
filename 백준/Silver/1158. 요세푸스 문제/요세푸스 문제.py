cfd07c403572668f060543ad97fcbcaa94668e2e
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2024-02-05 16:55:09 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2760 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,36 +1,28 @@
 import sys
+from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-data = []
-check = [0 for _ in range(n)]
+n,k = map(int,sys.stdin.readline().split())
+queue = deque()
 
-for i in range(1, n + 1):
-    data.append(i)
+for i in range(n):
+  queue.append(i+1)
+  
+answer = ['<']
+while(True):
+  if len(queue) == 0:
+    break
 
-count = 0
-index = 0
-result = []
-while True:
-    if len(result) == n:
-        break
+  for i in range(k-1):
+    target = queue.popleft()
+    queue.append(target)
 
-    if check[index] == 0:
-        count += 1
-        if count == k:
-            check[index] = 1
-            count = 0
-            result.append(data[index])
-    index += 1
+  target = queue.popleft()
+  answer.append(target)
+  
+  if len(queue) == 0:
+    continue
+  answer.append(', ')
 
-    if index >= n:
-        index = index % n
-
-answer = '<'
-for i in result:
-    answer += str(i)
-    if i == result[-1]:
-        answer += ">"
-    else:
-        answer += ", "
-
-print(answer)
+answer.append('>')
+for i in answer:
+  print(i, end="")
