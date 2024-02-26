d2c2f0a3c4cc36c67a9fd68e6d0ffb7ba15778b9
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2024-02-27 00:30:18 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+data = list(map(int, sys.stdin.readline().split()))
+
+dp = [[num] for num in data]
+
+for i in range(n):
+    for j in range(i):
+        if data[i] <= data[j]:
+            continue
+        if len(dp[j]) + 1 > len(dp[i]):
+            dp[i] = dp[j] + [data[i]]
+
+answer = 0
+index = -1
+for i in range(n):
+    if answer < len(dp[i]):
+        index = i
+        answer = len(dp[i])
+print(answer)
+for data in dp[index]:
+    print(data, end=" ")
