ef437bdfce093d4c179d5368381d0579189c448a
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2024-03-19 09:14:57 +0900

    [Gold IV] Title: 치즈, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -4,45 +4,40 @@ import collections
 
 n, m = map(int, sys.stdin.readline().split())
 graph = []
+queue = deque()
 
 for _ in range(n):
-    data = list(map(int, sys.stdin.readline().split()))
-    graph.append(data)
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
-queue = deque()
+dic = collections.defaultdict(int)
+queue.append((0, 0, 0))
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 check = [[0 for _ in range(m)] for _ in range(n)]
 
-totalTime = 0
-
-queue.append((0, 0, 0))
-check[0][0] = 1
-dic = collections.defaultdict(list)
-
 
 def bfs():
-    global totalTime
     while queue:
-        x, y, cnt = queue.popleft()
-        totalTime = max(totalTime, cnt)
+        x, y, count = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            if check[nx][ny] == 0 and graph[nx][ny] == 0:
+
+            if graph[nx][ny] == 0 and check[nx][ny] == 0:
                 check[nx][ny] = 1
-                queue.appendleft((nx, ny, cnt))
-                continue
+                queue.appendleft((nx, ny, count))
+
             if check[nx][ny] == 0 and graph[nx][ny] == 1:
-                graph[nx][ny] = 0
                 check[nx][ny] = 1
-                queue.append((nx, ny, cnt + 1))
-                dic[cnt].append((nx, ny))
+                graph[nx][ny] = 0
+                queue.append((nx, ny, count + 1))
+                dic[count] += 1
 
 
 bfs()
-print(totalTime)
-print(len(dic[totalTime-1]))
+time = sorted(dic)[-1] + 1
+print(time)
+print(dic[time - 1])
