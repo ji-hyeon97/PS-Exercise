06cec7bdc4abcacc94f9fea2f45482d444be492c
JiHyeon <79920930+ji-hyeon97@users.noreply.github.com>
2024-04-18 09:27:50 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 2296 ms, Memory: 155560 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,38 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+data = list(map(int, sys.stdin.readline().split()))
+
+lis = [data[0]]
+
+
+def lowerBound(target):
+    left = 0
+    right = len(lis) - 1
+    mid = (left + right) // 2
+
+    if lis[right] < target:
+        return -1
+    if lis[left] >= target:
+        return 0
+
+    while left + 1 < right:
+        if lis[mid] < target:
+            left = mid
+        else:
+            right = mid
+        mid = (left + right) // 2
+
+    return right
+
+
+for i in range(1, n):
+    index = lowerBound(data[i])
+
+    if index == -1:
+        lis.append(data[i])
+        continue
+
+    lis[index] = data[i]
+
+print(len(lis))
